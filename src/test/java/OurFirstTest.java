e97000819087f8b310a99ce87f342869b637d380
Dmitrijs <dmitrijs.braslis@gmail.com>
2019-09-01 13:27:34 +0300

    Initial commit

---
+++ src/test/java/OurFirstTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OurFirstTest {
+
+    private Integer sum(Integer a, Integer b) {
+        return a + b;
+    }
+
+    @Test
+    public void showSum() {
+        Integer c;
+        Integer d;
+
+        c = sum(15, 5);
+        d = sum(25, 15);
+
+//        System.out.println("C is: " + c + " \nand D is: " + d);
+
+        Assertions.assertEquals(21, c, "Shit!!!");
+    }
+}
